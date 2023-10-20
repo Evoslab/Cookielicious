b04a8da8792deccbc96b402b533a3146164d1106
Uraneptus <uraneptus33@gmail.com>
2023-10-20 17:30:42 +0200

    switched fully to datagen

--- src/main/java/com/evoslab/cookielicious/core/Cookielicious.java
+++ src/main/java/com/evoslab/cookielicious/core/Cookielicious.java
@@ -1,11 +1,14 @@
 package com.evoslab.cookielicious.core;
 
-import com.evoslab.cookielicious.core.datagen.CItemModelProvider;
-import com.evoslab.cookielicious.core.datagen.CLangProvider;
+import com.evoslab.cookielicious.core.datagen.client.CBlockStateProvider;
+import com.evoslab.cookielicious.core.datagen.client.CItemModelProvider;
+import com.evoslab.cookielicious.core.datagen.client.CLangProvider;
+import com.evoslab.cookielicious.core.datagen.server.CItemTagsProvider;
+import com.evoslab.cookielicious.core.datagen.server.CLootTableProvider;
 import com.evoslab.cookielicious.core.other.CookieliciousCompat;
 import com.evoslab.cookielicious.core.registry.CookieliciousLootConditions;
-import com.evoslab.cookielicious.core.datagen.CBlockTagProvider;
-import com.evoslab.cookielicious.core.datagen.CRecipeProvider;
+import com.evoslab.cookielicious.core.datagen.server.CBlockTagsProvider;
+import com.evoslab.cookielicious.core.datagen.server.CRecipeProvider;
 import com.teamabnormals.blueprint.core.util.registry.RegistryHelper;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
@@ -13,6 +16,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -21,6 +25,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Cookielicious.MOD_ID)
+@Mod.EventBusSubscriber(modid = Cookielicious.MOD_ID)
 public class Cookielicious {
     public static final String MOD_ID = "cookielicious";
     public static ResourceLocation modPrefix(String path) {
@@ -49,18 +54,22 @@ public class Cookielicious {
     private void doClientStuff(final FMLClientSetupEvent event) {
     }
 
-    private void gatherData(GatherDataEvent event) {
+    @SubscribeEvent
+    public void gatherData(GatherDataEvent event) {
         boolean client = event.includeClient();
         boolean server = event.includeServer();
-        DataGenerator dataGen = event.getGenerator();
+        DataGenerator generator = event.getGenerator();
         ExistingFileHelper fileHelper = event.getExistingFileHelper();
 
-        dataGen.addProvider(client, new CLangProvider(dataGen));
-        dataGen.addProvider(client, new CItemModelProvider(dataGen, fileHelper));
+        generator.addProvider(client, new CBlockStateProvider(generator, fileHelper));
+        generator.addProvider(client, new CItemModelProvider(generator, fileHelper));
+        generator.addProvider(client, new CLangProvider(generator));
 
-        dataGen.addProvider(server, new CBlockTagProvider(dataGen, fileHelper));
-        dataGen.addProvider(server, new CRecipeProvider(dataGen));
-        //TODO add LootTableProvider
+        CBlockTagsProvider blockTagProvider = new CBlockTagsProvider(generator, fileHelper);
+        generator.addProvider(server, blockTagProvider);
+        generator.addProvider(server, new CItemTagsProvider(generator, blockTagProvider, fileHelper));
+        generator.addProvider(server, new CRecipeProvider(generator));
+        generator.addProvider(server, new CLootTableProvider(generator));
 
     }
 }
